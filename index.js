1f24b19936a50fe4ef397d0976b3ad7edc5b70ae
Oleksandr Honcharov <0976053529@ukr.net>
2023-02-07 18:49:57 +0200

    Updated to v2.

--- index.js
+++ index.js
@@ -1,79 +1,88 @@
-const config = require("./config.json");
-const axios = require("axios");
+import * as dotenv from 'dotenv';
+dotenv.config();
+import mp from 'minecraft-protocol';
 
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+import { ActivityType, Client, GatewayIntentBits } from 'discord.js';
 
-let status;
-let playersOnline;
-let playersMax;
-let serverName;
+const { ping } = mp;
 
-let i = 1;
+const { IP, PORT, DISCORD_TOKEN, SV, TIME } = process.env;
+import Localization from './en.json' assert { type: "json" };
 
-function start() {
-    axios.get(`https://mcapi.us/server/status?ip=${config.ip}` + (config.port ? `&port=${config.port}` : ``)).then((res) => {
-        if (res.status === 200) {
-            let data = res.data;
-            if (data.online) {
-                console.log(res.data.players.now)
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-                status = data.online;
+const updateStatus = async () => {
+    const data = await pingServer();
 
-                serverName = data.server.name;
-
-                playersOnline = data.players.now;
-                playersMax = data.players.max;
-
-                changeInfo()
-            } else {
-                status = data.online;
-                changeInfo()
-            }
-        } else {
-            console.log("<!-- Error -->");
-            console.log(res.statusText);
-        }
-    });
-}
-
-function changeInfo() {
-    if (status) {
-        console.log("<!-- INFO -->");
-        console.log(`Players: ${playersOnline}/${playersMax}`);
-        console.log(`Server name: ${serverName}`);
-        console.log(`Update: ${i}`);
-        if (playersOnline === 0) {
-            client.user.setStatus('idle');
-            client.user.setActivity(`На сервере нету игроков.`, {
-                type: 'PLAYING'
-            });
-        } else if (playersOnline <= playersMax - 1) {
-            client.user.setStatus('online');
-            client.user.setActivity(`${playersOnline}/${playersMax} игроков.`, {
-                type: 'PLAYING'
-            });
-        } else if (playersOnline === playersMax) {
-            client.user.setActivity(`Сервер полон.`, {
-                type: 'PLAYING'
-            });
-            client.user.setStatus('dnd');
-        }
-
-        i++;
-    } else {
-        console.log("[SERVER] Выключен.")
+    if (data.error) {
         client.user.setStatus('dnd');
-        client.user.setActivity(`Сервер выключен.`, {
-            type: 'PLAYING'
+        return client.user.setActivity(Localization.ServerDisabled, {
+            type: ActivityType.Playing
         });
     }
-}
+
+    const { players } = data;
+
+    if (players.online === 0) {
+        client.user.setStatus('idle');
+        client.user.setActivity(Localization.ServerIsEmpty, {
+            type: ActivityType.Playing
+        })
+    }
+
+    if (players.online === players.max) {
+        client.user.setStatus('dnd');
+        client.user.setActivity(Localization.ServerIsFull, {
+            type: ActivityType.Playing
+        });
+    }
+
+    if (players.online <= players.max - 1) {
+        client.user.setStatus('online');
+        client.user.setActivity(StringBundler(Localization.ServerHasPlayers, data), {
+            type: ActivityType.Playing
+        });
+    }
+
+    console.log(`[BOT] Refresh: ${new Date().toLocaleTimeString()}`)
+};
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    start();
-    setInterval(start, config.time * 1000 * 60);
+    console.log(`[BOT] Logged in as ${client.user.tag}!`);
 });
 
-client.login(config.token);
+client.login(DISCORD_TOKEN).then(() => {
+    console.log(`[BOT] Try to login with credentials...`)
+
+    client.user.setStatus('online');
+    client.user.setActivity('Bot starting..');
+});
+
+function StringBundler (string, data) {
+    const operators = {
+        op: data.players.online,
+        mp: data.players.max
+    };
+
+    const keys = Object.keys(operators);
+
+    keys.map((object, index) => {
+        string = string.replace(`{${keys[index]}}`, operators[keys[index]])
+    })
+
+    return string;
+}
+
+async function pingServer () {
+    return await ping({
+        host: IP,
+        port: PORT,
+        version: SV
+    }).catch(() => {
+        return { error: true };
+    })
+}
+
+setInterval(async () => {
+    await updateStatus()
+}, TIME * 60 * 1000);
